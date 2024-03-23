02d39a773c686bee94f65eb44ab92aad814f03b4
Dimasik322 <dimakonovalov88@gmail.com>
2024-03-23 19:46:33 +0400

    task2

--- lab_1/caesar.py
+++ lab_1/caesar.py
@@ -2,7 +2,7 @@
 def caesar_encrypt(file_name : str, shift : int) -> None:
     with open(file_name, 'r', encoding='utf-8') as file:
         with open(f'lab_1/task1_encrypted.txt', 'w', encoding='utf-8') as new_file:
-            for char in file.read():
+            for char in file.read().replace(' ', ''):
                 if ord('а') <= ord(char.lower()) <= ord('я'):
                     new_file.write(chr((ord(char.lower()) + shift - ord('а')) % 32 + ord('а')))
                 else:
--- lab_1/route_transposition.py
+++ lab_1/route_transposition.py
@@ -9,7 +9,7 @@ def route_transposition(file_name : str, key : str):
     width = max(route) + 1
     with open(file_name, 'r', encoding='utf-8') as file:
         with open(f'lab_1/task1_encrypted.txt', 'w', encoding='utf-8') as new_file:
-            text = file.read()
+            text = file.read().replace(' ', '').replace('\n', '')
             height = int((len(text)) / (width + 1)) + 1
             matrix = [[text[((width * j) + i) % len(text)] for i in range(width)] for j in range(height)]
             #print(matrix)
