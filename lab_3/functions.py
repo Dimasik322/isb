b1bed7f74a112ad629804441a2142284b9d3fad5
Dimasik322 <dimakonovalov88@gmail.com>
2024-04-28 11:54:52 +0400

    pep changes

--- lab_3/functions.py
+++ lab_3/functions.py
@@ -15,10 +15,10 @@ def write_bytes(path: str, data: bytes) -> None:
     :return: None.
     """
     try:
-        with open(path, 'wb') as file:
+        with open(path, "wb") as file:
             file.write(data)
     except Exception as exc:
-        logging.error(f'Writing binary data to file error: {exc}\n')
+        logging.error(f"Writing binary data to file error: {exc}\n")
 
 
 def read_bytes(path: str) -> bytes:
@@ -27,11 +27,11 @@ def read_bytes(path: str) -> bytes:
     :return: bytes.
     """
     try:
-        with open(path, 'rb') as file:
+        with open(path, "rb") as file:
             data = file.read()
         return data
     except Exception as exc:
-        logging.error(f'Reading binary data from file error: {exc}\n')
+        logging.error(f"Reading binary data from file error: {exc}\n")
 
 
 def write_txt(path: str, data: str) -> None:
@@ -41,10 +41,10 @@ def write_txt(path: str, data: str) -> None:
     :return: None.
     """
     try:
-        with open(path, 'w') as file:
+        with open(path, "w") as file:
             data = file.write(data)
     except Exception as exc:
-        logging.error(f'Writing string data to file error: {exc}\n')
+        logging.error(f"Writing string data to file error: {exc}\n")
 
 
 def read_txt(path: str) -> str:
@@ -53,11 +53,11 @@ def read_txt(path: str) -> str:
     :return: string.
     """
     try:
-        with open(path, 'r', encoding="utf_8_sig") as file:
+        with open(path, "r", encoding="utf_8_sig") as file:
             data = file.read()
         return data
     except Exception as exc:
-        logging.error(f'Reading string data from file error: {exc}\n')
+        logging.error(f"Reading string data from file error: {exc}\n")
 
 
 def json_reader(path: str) -> dict:
@@ -66,11 +66,11 @@ def json_reader(path: str) -> dict:
     :return: dict which contains keys and values from file.
     """
     try:
-        with open(path, 'r', encoding='utf-8') as file:
+        with open(path, "r", encoding="utf-8") as file:
             paths = json.load(file)
         return paths
     except Exception as exc:
-        logging.error(f'Reading json file error: {exc}\n')
+        logging.error(f"Reading json file error: {exc}\n")
 
 
 def parse() -> argparse.Namespace:
@@ -81,13 +81,40 @@ def parse() -> argparse.Namespace:
         paths = json_reader(PATHS)
         parser = argparse.ArgumentParser()
         group = parser.add_argument_group()
-        group.add_argument('-gen', '--generation', action='store_true', help='Запускает режим генерации ключей по заданному ключу(128/192/256)')
-        group.add_argument('-enc', '--encryption', action='store_true', help='Запускает режим шифрования файла по заданному пути')
-        group.add_argument('-dec', '--decryption', action='store_true', help='Запускает режим дешифрования файла по заданному пути')
-        group.add_argument('-len', type=int, default=256, help='Задает длину ключа для генерации')
-        group.add_argument('-pth', type=str, default=paths["text"], help='Задает путь файля для шифрования')
-        group.add_argument('-cpt', type=str, default=paths["encrypted_text"], help='Задает путь файля для дешифрования')
+        group.add_argument(
+            "-gen",
+            "--generation",
+            action="store_true",
+            help="Запускает режим генерации ключей по заданному ключу(128/192/256)",
+        )
+        group.add_argument(
+            "-enc",
+            "--encryption",
+            action="store_true",
+            help="Запускает режим шифрования файла по заданному пути",
+        )
+        group.add_argument(
+            "-dec",
+            "--decryption",
+            action="store_true",
+            help="Запускает режим дешифрования файла по заданному пути",
+        )
+        group.add_argument(
+            "-len", type=int, default=256, help="Задает длину ключа для генерации"
+        )
+        group.add_argument(
+            "-pth",
+            type=str,
+            default=paths["text"],
+            help="Задает путь файля для шифрования",
+        )
+        group.add_argument(
+            "-cpt",
+            type=str,
+            default=paths["encrypted_text"],
+            help="Задает путь файля для дешифрования",
+        )
         args = parser.parse_args()
         return args
     except Exception as exc:
-        logging.error(f'Parsing error: {exc}\n')
+        logging.error(f"Parsing error: {exc}\n")
--- lab_3/main.py
+++ lab_3/main.py
@@ -3,9 +3,11 @@ from functions import json_reader, parse
 from constants import PATHS
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     paths = json_reader(PATHS)
-    crypto_system = HybridCryptography(paths["sym_path"], paths["private_path"], paths["public_path"])
+    crypto_system = HybridCryptography(
+        paths["sym_path"], paths["private_path"], paths["public_path"]
+    )
     args = parse()
     if args.generation:
         crypto_system.generate_keys(int(args.len / 8))
