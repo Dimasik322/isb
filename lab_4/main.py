5942ce5cf481b3525c3589c62d410df0d974db6a
Dimasik322 <dimakonovalov88@gmail.com>
2024-05-18 15:21:15 +0400

    luhn algorithm

--- lab_4/main.py
+++ lab_4/main.py
@@ -6,5 +6,5 @@ from constants import PATHS, DATA
 if __name__ == "__main__":
     data = json_reader(DATA)
     paths = json_reader(PATHS)
-    write_txt(paths["card_id_path"], HashOperating.get_id_by_hash(data["hash"], data["last_digits"], data["bin"]))
- 
+    #write_txt(paths["card_id_path"], HashOperating.get_id_by_hash(data["hash"], data["last_digits"], data["bin"]))
+    print(HashOperating.luhn_alg('43210987654328'))
--- lab_4/task.py
+++ lab_4/task.py
@@ -14,24 +14,32 @@ class HashOperating:
     """
     """
 
-    def card_id_operate(self, hash: str, last_digits: str, bin: tuple, middle_digits: int) -> str:
+    def card_id_operate(hash: str, last_digits: str, bin: tuple, middle_digits: int) -> str:
         for first_digits in bin:
             card_id = f"{first_digits}{middle_digits:06d}{last_digits}"
             if hashlib.sha512(card_id.encode()).hexdigest() == hash:
                 return card_id
 
-    def get_id_by_hash(self, hash: str, last_digits: str, bin: tuple) -> str:
+    def get_id_by_hash(hash: str, last_digits: str, bin: tuple) -> str:
         """Calculates card id number by hash, last_digits and bin-number.
         [BBBBBB][MMMMMM][LLLL]
         """
         with mp.Pool(processes=mp.cpu_count()) as p:
-            for result in p.starmap(self.card_id_operate, ((hash, last_digits, bin, middle_digits) for middle_digits in range(0, 1000000))):
+            for result in p.starmap(HashOperating.card_id_operate, ((hash, last_digits, bin, middle_digits) for middle_digits in range(0, 1000000))):
                 if result:
                     p.terminate()
                     break
         return result
             
-    #def lunh():
+    def luhn_alg(card_id: str) -> bool:
+        sum = 0
+        for digit in card_id[:-1:2]:
+            print((int(digit) * 2 % 10) + (int(digit) * 2 // 10))
+            sum += (int(digit) * 2 % 10) + (int(digit) * 2 // 10)
+        for digit in card_id[1:-1:2]:
+            print(digit)
+            sum += int(digit)
+        return (10 - sum % 10) == int(card_id[-1])
     
     #def collision_time():
 
